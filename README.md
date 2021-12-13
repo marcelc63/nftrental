8ef3c161e72fb4b8d8badd39b5e8a0a44b0bfe4f
Marcel <marcelc6363@gmail.com>
2021-12-13 23:31:30 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Solidity Contract Template based on
+
+Source: [https://etherscan.io/address/](https://etherscan.io/address/)
+
+Add Introduction Here
+
+Add Summary Here
+
+Curated by [@marcelc63](https://twitter.com/marcelc63) - [marcelchristianis.com](https://marcelchristianis.com)
+Each functions have been annotated based on my own research.
+
+Feel free to use and modify as you see appropriate
+
+# Using the Template
+
+Please do the followings
+
+1. Modify any code that's labeled with TODO
+2. Change the baseURI in deploy.js, run.js, and test.js
+3. Make sure to create your own .env and modify hardhat.config.js accordingly with your deployment URL and accounts private key.
+4. Remove the comments and annotation
+
+# Included in the Template
+
+1. Contract that CoolCatsNFT used
+2. Complete commentary on core functions
+3. Deployment Script
+4. Testing Script
+
+# Commands to use
+
+```
+// To test
+
+npx hardhat run scripts/run.js
+npx hardhat test
+
+// To deploy to Rinkeby testnet
+
+npx hardhart run scripts/deploy.js --network rinkeby
+```
---
+++ contracts/Template.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/* 
+Limitations
+all pass can only be rented out for 1 month */
+
+contract RentContract {
+  uint256 totalStake = 0;
+  uint256 rentCount = 0;
+  uint256 price = 0.1 ether;
+  uint80 latestExpiry;
+
+  struct Stake {
+    uint16 tokenId;
+    address owner;
+    uint80 time;
+    uint256 rentCount;
+  }
+
+  struct Rent {
+    address renter;
+    uint256 price;
+    uint80 time;
+    uint256 rentCount;
+    uint256 totalStake;
+  }
+
+  mapping(uint16 => Stake) stakes;
+  mapping(uint256 => Rent) rents;
+  mapping(address => uint256) renterToRent;
+
+  constructor() {}
+
+  function stake(uint16 tokenId) public {
+    stakes[tokenId] = Stake({
+      tokenId: tokenId,
+      owner: msg.sender,
+      time: uint80(block.timestamp),
+      rentCount: rentCount
+    });
+    totalStake += 1;
+  }
+
+  function rent() public {
+    rentCount += 1;
+    rents[rentCount] = Rent({
+      renter: msg.sender,
+      price: price,
+      time: uint80(block.timestamp),
+      rentCount: rentCount,
+      totalStake: totalStake
+    });
+    renterToRent[msg.sender] = rentCount;
+    latestExpiry = uint80(block.timestamp + 30 days);
+  }
+
+  function isRent() public view returns (bool) {
+    uint256 renter = renterToRent[msg.sender];
+    Rent memory renterRent = rents[renter];
+    return renterRent.time < renterRent.time + 30 days;
+  }
+
+  function unstake(uint16 tokenId) public {
+    Stake memory ownerStake = stakes[tokenId];
+    if (ownerStake.time < latestExpiry) {
+      uint256 payout;
+      for (uint256 i = ownerStake.rentCount; i < rentCount; i++) {
+        Rent memory renterRent = rents[i];
+        payout += renterRent.price / totalStake;
+      }
+      totalStake -= 1;
+      delete stakes[tokenId];
+    }
+  }
+
+  function claim(uint16 tokenId) public {
+    Stake memory ownerStake = stakes[tokenId];
+    if (ownerStake.time < latestExpiry) {
+      uint256 payout;
+      for (uint256 i = ownerStake.rentCount; i < rentCount; i++) {
+        Rent memory renterRent = rents[i];
+        payout += renterRent.price / totalStake;
+      }
+
+      stakes[tokenId] = Stake({
+        tokenId: ownerStake.tokenId,
+        owner: ownerStake.owner,
+        time: uint80(block.timestamp),
+        rentCount: rentCount
+      });
+    }
+  }
+}
